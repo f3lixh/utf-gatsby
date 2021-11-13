fd8da70424fddb3100550f2f262cba7804888bd8
f3lixh <47897037+f3lixh@users.noreply.github.com>
2021-11-13 16:33:13 +0100

    No Sort

--- src/pages/custom.js
+++ src/pages/custom.js
@@ -1,24 +1,55 @@
 import * as React from "react";
 import Layout from "../components/Layout";
+import { Link, graphql, useStaticQuery } from "gatsby";
+import JSONData from "../data/fonts.json";
+import { useState } from "react";
 
 // Step 2: Define your component
 const Custom = () => {
+  const [fontList, setFontList] = useState(JSONData.fonts);
+
+  const data = useStaticQuery(graphql`
+    query {
+      allFile(filter: { ext: { eq: ".svg" } }) {
+        edges {
+          node {
+            name
+            publicURL
+          }
+        }
+      }
+    }
+  `);
+
+  function getPublicURL(fontName) {
+    return data.allFile.edges.find((font) => {
+      return font.node.name === fontName;
+    }).node.publicURL;
+  }
+
   return (
     <Layout pageTitle="Custom" pageIndex="501">
-      <h1>Custom Typeface</h1>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed
-        perferendis consequatur odio natus amet delectus ut at placeat illo
-        tempora exercitationem officia eius distinctio nam error architecto
-        quasi omnis libero ipsam beatae optio, consequuntur minus iste?
-        Consequuntur similique fugit voluptatibus vero pariatur a dolorum
-        dolores tenetur officiis asperiores! Obcaecati eius eaque et debitis
-        nostrum laboriosam, harum aliquam eveniet exercitationem iste rem sequi
-        earum deleniti autem! Ipsum iste tempore facere, itaque nisi error ipsam
-        accusamus nesciunt quod. Nostrum ut, aperiam ad dolorum officia quia
-        praesentium repellat quis voluptatem, blanditiis debitis necessitatibus
-        asperiores placeat error? Quidem rerum, nemo animi dicta quos repellat!
-      </p>
+      {fontList.map((font, index) => {
+        return (
+          <div key={index}>
+            <img src={getPublicURL(font.slug)} alt={font.name} />
+            <p>
+              {font.name} {font.slug}
+            </p>
+          </div>
+        );
+      })}
+      <div>BREAKa</div>
+      {JSONData.fonts.map((font, index) => {
+        return (
+          <div>
+            <img src={getPublicURL(font.slug)} alt={font.name} />
+            <p>
+              {font.name} {font.slug}
+            </p>
+          </div>
+        );
+      })}
     </Layout>
   );
 };
